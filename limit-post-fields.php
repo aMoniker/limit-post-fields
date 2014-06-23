96b590955c48de4e202a4cedda4df1ba7bb29454
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-22 22:53:55 -0400

    init commit

---
+++ limit-post-fields.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Limit Post Fields
+Version: 0.1-alpha
+Description: Limit the length of post fields, meta fields, and ACF fields.
+Author: Jim Greenleaf
+Author URI: http://jim.greenle.af
+Plugin URI: TBD
+Text Domain: limit-post-fields
+Domain Path: /languages
+License: AGPL3 http://www.gnu.org/licenses/agpl.html
+*/
