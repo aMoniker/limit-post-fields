be89940de634054a0de9d8fb67e42a3dc9c7574f
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-06-29 20:30:37 -0400

    basic saving/loading of settings

--- inc/settings-page.php
+++ inc/settings-page.php
@@ -2,8 +2,8 @@
 
 final class LimitPostFieldsSettings
 {
-    private $rules;
     private $page;
+    private $menu_slug = 'limit-post-fields';
     private $valid_fields = array();
 
     public function __construct()
@@ -11,8 +11,6 @@ final class LimitPostFieldsSettings
         add_action('admin_enqueue_scripts', array($this, 'settings_assets'));
         add_action('admin_menu', array($this, 'settings_menu'));
         add_action('admin_init', array($this, 'settings_fields'));
-
-        $this->rules = LimitPostFields::get_rules();
     }
 
     public function settings_assets($page) {
@@ -26,40 +24,28 @@ final class LimitPostFieldsSettings
         );
     }
 
-    public function settings_footer()
-    {
-        ?><script type="text/javascript">
-            window.limit_settings_fields = window.limit_settings_fields || {};
-            window.limit_settings_fields.valid_fields = <?php
-                echo json_encode($this->valid_fields);
-            ?>;
-            console.log('limit settings fields', window.limit_settings_fields);
-        </script><?php
-    }
-
     public function settings_menu()
     {
-        $this->page = add_options_page('Limit Post Fields Settings', 'Limit Post Fields', 'manage_options', 'limit-post-fields', array($this, 'settings_page'));
-
-        add_action("admin_footer-{$this->page}", array($this, 'settings_footer'));
+        $this->page = add_options_page('Limit Post Fields Settings', 'Limit Post Fields', 'manage_options', $this->menu_slug, array($this, 'settings_page'));
     }
 
     public function settings_page()
     {
         ?><h2>Limit Post Fields</h2>
-        <form method="POST" action=""><?php
-            do_settings_sections('limit-post-fields');
+        <form method="POST" action="options.php"><?php
+            settings_fields($this->menu_slug);
+            do_settings_sections($this->menu_slug);
             submit_button(); ?>
         </form><?php
     }
 
-    public function settings_fields($value='')
+    public function settings_fields()
     {
         add_settings_section(
             'limitpostfields_settings_main',
-            'Field Limits',
+            '',
             array($this, 'setting_section_header'),
-            'limit-post-fields'
+            $this->menu_slug
         );
 
         $post_types = LimitPostFields::get_valid_post_types();
@@ -68,16 +54,16 @@ final class LimitPostFieldsSettings
             $pto = get_post_type_object($post_type);
             if (!$pto) { continue; } // the pto has disbanded
 
-            $settings_field_name = "limitsettingsfields_posttype_$post_type";
+            $settings_field_name = $this->get_settings_field_name($post_type);
 
             add_settings_field(
                 $settings_field_name,
                 $pto->label,
                 array($this, "handle_settings_field_$post_type"),
-                'limit-post-fields',
+                $this->menu_slug,
                 'limitpostfields_settings_main'
             );
-            register_setting('limit-post-fields', $settings_field_name);
+            register_setting($this->menu_slug, $settings_field_name);
         }
     }
 
@@ -98,6 +84,9 @@ final class LimitPostFieldsSettings
     {
         $div_id = "limitpostfields_field_type_$name";
         $valid_fields = LimitPostFields::get_valid_post_type_fields($name);
+        $settings_field_name = $this->get_settings_field_name($name);
+
+        $saved_settings = get_option($settings_field_name);
 
         if (!isset($this->valid_fields[$name])) {
             $this->valid_fields[$name] = array();
@@ -111,17 +100,26 @@ final class LimitPostFieldsSettings
         ?><div id="<?php echo $div_id; ?>">
             <div class="field-limits"><?php
                 foreach ($this->valid_fields[$name] as $fields_type => $fields):
-                    foreach ($fields as $field): ?>
-                    <div class="field-limit">
+                    foreach ($fields as $field):
+                    $value = isset($saved_settings[$field])
+                           ? $saved_settings[$field]
+                           : ''
+                           ;
+                    ?><div class="field-limit">
                         <label>
                             <span class="title"><?php echo $field; ?></span>
-                            <input type="number" class="limit-input" name="limitpostfields_limit[<?php echo $field; ?>]" step="1" min="0" placeholder="0">
+                            <input type="number" class="limit-input" name="<?php echo $settings_field_name; ?>[<?php echo $field; ?>]" step="1" min="0" placeholder="0" value="<?php echo $value; ?>">
                         </label>
                     </div><?php
                     endforeach;
                 endforeach; ?>
             </div>
         </div><?php
+    }
+
+    private function get_settings_field_name($post_type)
+    {
+        return "limitsettingsfields_posttype_$post_type";
     }
 }
 
