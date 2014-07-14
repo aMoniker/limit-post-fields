82d27a074bf02db976986732cc6995e43d52381f
Jim Greenleaf <james.a.greenleaf@gmail.com>
2014-07-14 00:48:08 -0400

    rewrite js to use classes an inheritance

--- js/edit-page.js
+++ js/edit-page.js
@@ -2,161 +2,197 @@ jQuery(function($) {
 
 if (!window.limitpostfields || !window.limitpostfields.settings) { return; }
 
-var field_rules = {
-    title: {
-        element: '#title',
-        after_element: '#title',
-        create_callback: function(e, ui) {
-            $('#title').attr('maxlength', get_max_length('title'));
-        },
-        update_watcher: function(update_callback) {
-            $('#title').on('change keyup', function() {
-                update_callback('title', $(this).val().length);
-            });
-            $('#title').trigger('keyup');
-        }
-    },
-    editor: {
-        after_element: '#postdivrich',
-        create_callback: function(e, ui) {
-            var bottom_margin = $('#postdivrich').css('margin-bottom');
-            $('#postdivrich').css({ 'margin-bottom': '0' });
-            $(this).css('margin-bottom', bottom_margin);
-            $('#content').attr('maxlength', get_max_length('editor'));
-        },
-        update_watcher: function(update_callback) {
-            var tinymce_interval;
-            tinymce_interval = setInterval(function() {
-                if (tinyMCE.editors && tinyMCE.editors.length) {
-                    if (init_postcontent_watcher()) {
-                        clearInterval(tinymce_interval);
-                    }
-                }
-            }, 100);
+var LimitedField = Class.extend({
+     bar_element: undefined
+    ,element: undefined
+    ,progress_bar_html: [
+        '<div class="progress-bar limitpostfields">',
+            '<span class="progress-count">',
+                '<span class="progress-used">0</span><span class="progress-total"></span>',
+            '</span>',
+        '</div>',
+    ].join('\n')
+    ,init: function(max_length) {
+        this.max_length = max_length;
 
-            var tinyMCE_handler = function(ed, e) {
-                update_callback('editor', get_trimmed_length(ed));
-            };
+        var $progress_bar = $(this.progress_bar_html).progressbar({
+            max: this.max_length,
+            value: 0,
+            create: $.proxy(this.create_callback || $.noop, this)
+        });
 
-            var get_trimmed_length = function(ed) {
-                var content = ed.getContent({ format: 'text' });
-                content = content.replace(/\n/, '');
-                return content.length;
-            };
+        $progress_bar.find('.progress-total').text(this.max_length);
 
-            var init_postcontent_watcher = function() {
-                var editor_hooked = false;
-                $.each(tinyMCE.editors, function(i, ed) {
-                    if (ed.id && ed.id === 'content') {
-                        editor_hooked = true;
+        $(this.element).after($progress_bar);
 
-                        var unique_dom_id = tinymce.DOM.uniqueId();
-                        var $ed_dom = $(ed.dom.doc);
-
-                        var max_length = get_max_length('editor');
-                        var old_length = get_trimmed_length(ed);
-                        var last_content;
-                        var keyup_finished = true;
-
-                        ed.on('KeyUp', function(e) {
-                            var new_length = get_trimmed_length(ed);
-                            if (new_length > max_length && new_length > old_length) {
-                                ed.setContent(last_content);
-
-                                // hack to put the cursor back into the right place
-                                ed.focus();
-                                ed.selection.select(ed.dom.select('.'+unique_dom_id)[0]);
-                                ed.selection.collapse(0);
-                            }
-
-                            // remove the cursor helper
-                            $ed_dom.find('.' + unique_dom_id).remove();
-
-                            last_content = ed.getContent();
-                            old_length = get_trimmed_length(ed);
-
-                            keyup_finished = true;
-                            tinyMCE_handler(ed);
-                        });
-
-                        ed.on('KeyDown', function(e) {
-                            tinyMCE_handler(ed);
-
-                            if (!keyup_finished) { return; }
-                            keyup_finished = false;
-
-                            // add the cursor helper
-                            ed.insertContent(
-                                '<span class="' +unique_dom_id+ '" '
-                              + 'style="display:none;">placeholder</span>'
-                            );
-
-                            last_content = ed.getContent();
-                        });
-
-                        ed.on('Paste', function(e) {
-                            tinyMCE_handler(ed);
-                        });
-
-                        tinyMCE_handler(ed);
-                    }
-                });
-
-                return editor_hooked;
-            }
-
-            $('#content').on('change keyup', function() {
-                update_callback('editor', $(this).val().length);
-            });
-            $('#content').trigger('keyup');
-        }
-    },
-    excerpt: {
-        after_element: '#postexcerpt',
-        create_callback: function(e, ui) {
-            $('#excerpt').attr('maxlength', get_max_length('excerpt'));
-            var bottom_margin = $('#postexcerpt').css('margin-bottom');
-            $('#postexcerpt').css({ 'margin-bottom': '0' });
-            $(this).css('margin-bottom', bottom_margin);
-        },
-        update_watcher: function(update_callback) {
-            $('#excerpt').on('change keyup', function() {
-                update_callback('excerpt', $(this).val().length);
-            });
-            $('#excerpt').trigger('keyup');
-        }
+        this.update_watcher($.proxy(function(length) {
+            length = +length;
+            $progress_bar
+                .progressbar('value', length)
+                .toggleClass('empty', length === 0)
+                .toggleClass('full', length === this.max_length)
+                .toggleClass('overfilled', length > this.max_length)
+                .find('.progress-used').text(length)
+                ;
+        }, this));
     }
-};
+    ,validate_length: function(length) {
+        if (!length)                  { return true;  }
+        if (!this.max_length)         { return true;  }
+        if (length > this.max_length) { return false; }
+        return true;
+    }
+    ,create_callback: function(e, ui) {}
+    ,update_watcher: function(callback) {}
+});
 
+var LimitedTitleField = LimitedField.extend({
+     element: '#title'
+    ,create_callback: function(e, ui) {
+        $(this.element).attr('maxlength', this.max_length);
+        this._super();
+    }
+    ,update_watcher: function(callback) {
+        $(this.element)
+            .on('change keyup', function() {
+                callback($(this).val().length);
+            })
+            .trigger('keyup')
+            ;
+    }
+});
 
-// utility function to check if the current length is valid
-var validate_field_length = function(field_name, length) {
-    var max_length = get_max_length(field_name);
+var LimitedContentField = LimitedField.extend({
+     element: '#postdivrich'
+    ,text_element: '#content'
+    ,create_callback: function(e, ui) {
+        var $element = $(this.element);
+        var bottom_margin = $element.css('margin-bottom');
 
-    if (!length)              { return true;  }
-    if (!max_length)          { return true;  }
-    if (length > +max_length) { return false; }
+        $element.css({ 'margin-bottom': '0' });
 
-    return true;
-};
+        console.log('create callback', e, ui);
+        $(e.target).css('margin-bottom', bottom_margin);
+        $(this.text_element).attr('maxlength', this.max_length);
 
-var get_max_length = function(field_name) {
-    if (!field_name) { return undefined; }
+        this._super();
+    }
+    ,update_watcher: function(callback) {
+        this.update_callback = callback;
 
-    var max_length = +window.limitpostfields.settings[field_name];
-    if (!max_length) { return undefined; }
+        var tinymce_interval;
+        tinymce_interval = setInterval($.proxy(function() {
+            if (tinyMCE.editors && tinyMCE.editors.length) {
+                if (this.init_postcontent_watcher()) {
+                    clearInterval(tinymce_interval);
+                }
+            }
+        }, this), 100);
 
-    return max_length;
-};
+        $(this.text_element)
+            .on('change keyup', function() {
+                callback($(this).val().length);
+            })
+            .trigger('keyup')
+            ;
+    }
+    ,init_postcontent_watcher: function() {
+        var editor_hooked = false;
 
-// the progress bar template
-var $progress_bar = $([
-    '<div class="progress-bar limitpostfields">',
-        '<span class="progress-count">',
-            '<span class="progress-used">0</span><span class="progress-total"></span>',
-        '</span>',
-    '</div>',
-].join('\n'));
+        $.each(tinyMCE.editors, $.proxy(function(i, ed) {
+            if (!ed.id || ed.id !== 'content') { return; }
+
+            editor_hooked = true;
+
+            var unique_dom_id = tinymce.DOM.uniqueId();
+            var $ed_dom = $(ed.dom.doc);
+
+            var old_length = this.tinymce_get_trimmed_length(ed);
+            var last_content;
+            var keyup_finished = true;
+
+            ed.on('KeyDown', $.proxy(function(e) {
+                if (e.metaKey
+                 || e.shiftKey
+                 || e.ctrlKey
+                 || e.altKey
+                 || e.altGraphKey
+                ) {
+                    return;
+                }
+
+                this.tinymce_update_handler(ed);
+
+                if (!keyup_finished) { return; }
+                keyup_finished = false;
+
+                ed.insertContent( // add the cursor hack
+                    '<span class="' +unique_dom_id+ '" '
+                  + 'style="display:none;">placeholder</span>'
+                );
+
+                last_content = ed.getContent();
+            }, this));
+
+            ed.on('KeyUp', $.proxy(function(e) {
+                var new_length = this.tinymce_get_trimmed_length(ed);
+                if (new_length > this.max_length && new_length > old_length) {
+                    ed.setContent(last_content);
+
+                    ed.focus(); // hack puts the cursor back
+                    ed.selection.select(ed.dom.select('.'+unique_dom_id)[0]);
+                    ed.selection.collapse(0);
+                }
+
+                $ed_dom.find('.' + unique_dom_id).remove(); // remove cursor hack
+
+                last_content = ed.getContent();
+                old_length = this.tinymce_get_trimmed_length(ed);
+
+                keyup_finished = true;
+                this.tinymce_update_handler(ed);
+            }, this));
+
+            ed.on('Paste', $.proxy(function(e) {
+                this.tinymce_update_handler(ed);
+            }, this));
+
+            this.tinymce_update_handler(ed); // init
+        }, this));
+
+        return editor_hooked;
+    }
+    ,tinymce_update_handler: function(ed, e) {
+        this.update_callback(this.tinymce_get_trimmed_length(ed));
+    }
+    ,tinymce_get_trimmed_length: function(ed) {
+        var content = ed.getContent({ format: 'text' });
+        content = content.replace(/\n/, '');
+        content = content.replace(/^\s+/, '');
+        content = content.replace(/\s+$/, '');
+        return content.length;
+    }
+});
+
+var LimitedExcerptField = LimitedField.extend({
+     element: '#postexcerpt'
+    ,text_element: '#excerpt'
+    ,create_callback: function(e, ui) {
+        $(this.text_element).attr('maxlength', this.max_length);
+        var bottom_margin = $(this.element).css('margin-bottom');
+        $(this.element).css({ 'margin-bottom': '0' });
+        $(e.target).css('margin-bottom', bottom_margin);
+    }
+    ,update_watcher: function(callback) {
+        $(this.text_element)
+            .on('change keyup', function() {
+                callback($(this).val().length);
+            })
+            .trigger('keyup')
+            ;
+    }
+});
+
 
 // initialize all the progressbars accoring to
 // the fields defined in the global object
@@ -164,28 +200,20 @@ $.each(window.limitpostfields.settings, function(field, limit) {
     limit = +limit;
     if (!limit) { return; }
 
-    var rules = field_rules[field];
-    if (!rules) { return; }
+    if (field.match(/^field_/)) { // acf field
+        var $field = $('[name="fields[' +field+ ']"]');
+        console.log('field', $field);
+    }
 
-    var $bar = $progress_bar.clone().progressbar({
-        max: limit,
-        value: 0,
-        create: rules.create_callback || $.noop
-    });
-
-    $bar.find('.progress-total').text(limit);
-
-    $(rules.after_element).after($bar);
-
-    rules.update_watcher(function(field_name, value) {
-        value = +value;
-        var maxlength = get_max_length(field_name);
-        $bar.progressbar('value', value);
-        $bar.find('.progress-used').text(value);
-        $bar.toggleClass('empty', value === 0);
-        $bar.toggleClass('full', value === maxlength);
-        $bar.toggleClass('overfilled', value > maxlength);
-    });
+    switch (field) {
+        case 'title':
+            new LimitedTitleField(limit); break;
+        case 'editor':
+            new LimitedContentField(limit); break;
+        case 'excerpt':
+            new LimitedExcerptField(limit); break;
+        default: break;
+    }
 });
 
 });
